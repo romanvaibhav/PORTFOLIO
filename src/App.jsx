6b250b57f3725336aaea5932f568f52d699d1ec8
romanvaibhav <vaibhavroman249@gmail.com>
2025-06-11 04:52:29 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import  First from './components/firstPage/first';
+import Second from './components/SecondPage/Second';
+
+function App() {
+
+  return (
+    <>
+    <div>
+      <First></First>
+      <Second></Second>
+    </div>
+   
+
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/firstPage/first.jsx
@@ -0,0 +1,21 @@
+import './first.css';
+function First() {
+  return (
+    <div className="gradient-bg">
+      <div>
+        <p >Jai Shree Ram, I'm </p>
+        <p className='name'>VAIBHAV</p>
+      </div>
+      <div className='photo'>
+        <div className='welcome'>
+          <p className='wel'>WELCOME</p>
+          <p className='to'>TO MY</p>
+          <p className='port'>PORTFOLIO</p>
+        </div>
+            <img className='img' src="/src/assets/WhatsApp Image 2025-05-27 at 10.50.05_f06d8c76-fotor-bg-remover-202506110341.png" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default First;
